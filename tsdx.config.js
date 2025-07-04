70e6e72812839d4b5f8f1c302d7df78b07c38590
iRewiewer <irewiewer@gmail.com>
2025-07-04 18:07:52 +0300

    v0.1.1

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,12 +1,11 @@
-// tsdx.config.js
 const postcss = require('rollup-plugin-postcss');
 
 module.exports = {
     rollup(config) {
         config.plugins.push(
             postcss({
-                inject: true,   // inlines your CSS
-                modules: false, // plain CSS, not modules
+                inject: true,
+                modules: false,
             })
         );
         return config;
